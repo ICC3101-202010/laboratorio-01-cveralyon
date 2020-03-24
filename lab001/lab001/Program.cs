4af417fde6241ee4359cc84fde82bf4f1970c7a0
cveral <cavera2@miuandes.cl>
2020-03-24 15:14:05 -0300

    version funcional

--- lab001/lab001/Program.cs
+++ lab001/lab001/Program.cs
@@ -2,12 +2,49 @@
 
 namespace lab001
 {
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-        }
-    }
+	// Clase Persona (check)
+	public class Person
+	{
+		String first;
+		String last;
+
+
+		// Constructor de clase Persona (check)
+		public Person(String first, String last)
+		{
+			this.first = first;
+			this.last = last;
+
+		}
+
+		// metodo Lanzar (check)
+		public int lanzar()
+		{
+			Random random = new Random();
+			return random.Next(0, 3);
+		}
+
+		// main del programa
+		public static void Main(String[] args)
+		{
+
+			// creacion de Bob Kunga
+			Person Bob = new Person("Bob", "Kunga");
+
+			Console.WriteLine(Bob.first, Bob.last, "jugo el Ca Chi Pun con:");
+
+
+
+            Console.WriteLine(Bob.lanzar());
+		}
+
+    
+
+
+
+	}
 }
+    
+
+
+
